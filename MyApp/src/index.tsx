dcc66d148e8072d17e4ab92f5892eda59a7e0543
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-11-22 17:52:51 +0900

    準備のためにApp.tsxとIndex.tsxを更新しました。

---
+++ MyApp/src/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * インデックスファイル
+ * @format
+ */
+
+import React from "react";
+import { View } from "react-native";
+import styles from "./StyleSheet";
+
+/**
+ * Indexコンポーネント
+ */
+function Index() {
+  return <View style={styles.container} />
+}
+
+export default Index
+
