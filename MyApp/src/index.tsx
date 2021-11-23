2d4087a9b3dba893127ec3e28ac513e6c2d46eb8
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-11-23 14:34:33 +0900

    Main画面とSub画面を実装しました。

--- MyApp/src/index.tsx
+++ MyApp/src/index.tsx
@@ -3,16 +3,61 @@
  * @format
  */
 
+import { useNavigation, NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 import styles from "./StyleSheet";
 
 /**
- * Indexコンポーネント
+ * Main画面用のコンポーネント
  */
-function Index() {
-  return <View style={styles.container} />
+function Main() {
+  const { navigate } = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text>Main</Text>
+      <TouchableOpacity onPress={() => { navigate('Sub'); }}>
+        <Text>go to sub</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-export default Index
+/**
+ * Sub画面用のコンポーネント
+ */
+function Sub() {
+  return (
+    <View style={styles.container}>
+      <Text>Sub</Text>
+    </View>
+  );
+}
 
+// createStackNavigator型の変数を生成する。
+const Stack = createStackNavigator();
+
+/**
+ * ルーティングを設定する関数
+ */
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Sub" component={Sub} />
+    </Stack.Navigator>
+  );
+}
+
+/**
+ * 外部に公開するコンポーネント
+ */
+export default function() {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+}
