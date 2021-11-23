7e8766cf046876d2059089c94bdbd15fe3ce4537
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-11-23 16:58:04 +0900

    Statisticsコンポーネントを追加

---
+++ MyApp/src/components/pages/Statistics/index.tsx
@@ -0,0 +1,22 @@
+/**
+ * Statistics画面用のコンポーネントファイル
+ */
+
+import React from "react";
+import { View, Text } from 'react-native';
+import styles from '../../../StyleSheet';
+
+/**
+ * Statisticsコンポーネントファイル
+ */
+function Statistics() {
+  return (
+    <View style={styles.container}>
+      <Text>Statistics</Text>
+    </View>
+  );
+}
+
+export default Statistics;
+
+
